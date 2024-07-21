cca440924dc5f90df8eaad8898a6e0810a76bff8
kamaleldinmostafa87 <kamaleldin.mostafa87@gmail.com>
2024-07-21 23:57:18 +0300

    initial creating server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.write('hamo')
+        res.end()
+    }else if(req.url === '/home'){
+        res.write('Hello from the home page')
+        res.end()
+    }
+})
+
+server.listen(3000)
